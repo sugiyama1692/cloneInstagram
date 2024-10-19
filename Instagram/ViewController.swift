33005b5d9b375ed09fcf9c736283734282c603a7
sugiyama1692 <kyoei.engine.mba2408.starlight@gmail.com>
2024-10-19 13:49:29 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by mba2408.starlight kyoei.engine on 2024/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
